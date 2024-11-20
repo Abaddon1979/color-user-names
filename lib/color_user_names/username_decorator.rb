d10257c33da5b92e459eb4b1650bbb03d585ed5c
Abaddon1979 <77430838+Abaddon1979@users.noreply.github.com>
2024-11-20 15:07:26 -0500

    Update username_decorator.rb

--- lib/color_user_names/username_decorator.rb
+++ lib/color_user_names/username_decorator.rb
@@ -1,19 +1,7 @@
 module ColorUserNames
   module UsernameDecorator
     def cooked(opts = nil)
-      original_cooked = super(opts)
-
-      doc = Nokogiri::HTML::DocumentFragment.parse(original_cooked)
-      doc.css('span.username').each do |span|
-        user = User.find_by(username: span.text)
-        if user
-          user.groups.order(:position).each do |group| # Maintain group order
-            span['class'] = span['class'] + " group-#{group.id}-colored-name"
-          end
-        end
-      end
-
-      doc.to_html.html_safe
+      super(opts) # Just return the original cooked username for now
     end
   end
 end
